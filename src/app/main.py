80a49d664021fd7b9fdb3e05df76af81b2c41fec
Cameron Sajedi <cameronsajedi@gmail.com>
2020-06-06 18:10:07 -0400

    fix tree proof logic

--- src/app/main.py
+++ src/app/main.py
@@ -19,14 +19,14 @@ merkle_tree = Tree()
 # RPC Methods
 
 @api_v1.method(errors=[ChecksumFormatError])
-def submit(checksum:bytes) -> bool:
+def submit(checksum:str) -> bool:
     """Expect a bytestring in hexadecimal representing the hash digest of the file you want to timestamp. The response will be a boolean indicating if the submission was accepted by the calendar (but the proof of existence is assumed incomplete). Digests submitted this block are idempotent - meaning that you can only timestamp a file once per block."""
     logger.info("Checksum {} submitted for inclusion", checksum)
     return merkle_tree.stamp(checksum)
 
 
 @api_v1.method(errors=[ChecksumFormatError, ChecksumNotFoundError])
-def proof(checksum:bytes) -> dict:
+def proof(checksum:str) -> dict:
     """Expects a bytestring already submitted. The response will be an existing proof upgraded to its latest commitment.Or an error indicating the checksum must be submitted first. This endpoint should be polled by submitters of incomplete timestamps for proofs that include the complete anchor.""" 
     logger.info("Checksum {} submitted for inclusion", checksum)
     return merkle_tree.proofFor(checksum)

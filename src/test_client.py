736d9811e67446d8abe9d24315ca091a92bddc26
Cameron Sajedi <cameronsajedi@gmail.com>
2020-06-08 14:31:57 -0400

    separate tests from demo client

--- src/test_client.py
+++
@@ -1,5 +0,0 @@
-"""Here we will test that a naieve user knowing only the hash function and endpoints can successfully hash their files and strings"""
-
-import requests
-
-
---
+++ src/tests/test_client.py
@@ -0,0 +1,5 @@
+"""Here we will test that a naive user knowing only the hash function and endpoints can successfully hash their files and strings"""
+
+import requests
+
+
